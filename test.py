fd77e7f20655fa9e48cb21012d3a463fac28d0f6
Solomon Gaim <sgaim@u.rochester.edu>
2015-10-09 12:14:08 -0400

    Updated test to include columns
    Parameters for a stream are as follows: 
    
    stream_name  = stream(name, supply_temp,target_temp, heat_capacity, dT)
    column_name = column(self,name, fixed_temp, heat, reb_cond,dT):
    
    Then create an array with all streams and process it to your desired extent using the process class functions

--- test.py
+++ test.py
@@ -1,18 +1,32 @@
-from stream_process import stream, process
+from Process_python import stream, process, column
 
 #dT Min
 dT = 10
 
 #stream# = (name, suppy_temp,target_temp, heat_capacity,dT)
-stream1 = stream(1,60,180,3,dT)
-stream2 = stream(2,180,40,2,dT)
-stream3 = stream(3,30,105,2.6,dT)
-stream4 = stream(4,150,40,4,dT)
+stream1 = stream(1,20,135,2,dT)
+stream2 = stream(2,170,60,3,dT)
+stream3 = stream(3,80,140,4,dT)
+stream4 = stream(4,150,30,1.5,dT)
 
-#combine streams into an array
-streams = [stream1, stream2, stream3, stream4]
 
-#Process stream array using the "process" class
+#Columns
+reb_1 = column('Reboiler_1',150,20,'reb',dT)
+cond_1 = column('Condensor_1',150,20,'cond',dT)
+
+
+#If you would like to add more streams
+'''
+stream5 = stream(5,60,180,3,None,dT)
+stream6 = stream(6,180,40,2,None,dT)
+stream7 = stream(7,30,105,2.6,None,dT)
+stream8 = stream(8,150,40,4,None,dT)
+'''
+
+#combine streams into one array
+streams = [stream1, stream2, stream3, stream4,reb_1,cond_1]
+
+#Process stream array in the "process" class
 p = process(streams)
 
 #Calculates mCp
@@ -26,6 +40,10 @@ p.pinch()
 
 #Prints Nessasary Info about the streams
 p.final()
+
+#integrates columns (Has to be after the feasible unintegrated cascade is determined)
+p.column_in()
+
 
 #Plots the All three composite curves and Grid Diagrams
 p.comp_curve()
